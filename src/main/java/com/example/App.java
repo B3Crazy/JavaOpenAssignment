510772b2b8d566d7219e779a6d2531ba0764a0d2
B3Crazy <paulszelag@gmail.com>
2025-01-06 18:54:41 +0100

    generatis basis of project

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,8 @@
 package com.example;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * JavaFX App
@@ -16,19 +12,11 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    public void start(Stage scene) throws Exception {
+        scene.setTitle("Calculator");
+        scene.setWidth(640);
+        scene.setHeight(480);
+        scene.show();
     }
 
     public static void main(String[] args) {
